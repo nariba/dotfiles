08c860b0505ec4e1163becc4c4de3d0892fc6c24
nariba <akira.naribayashi@gmail.com>
2017-07-09 01:54:50 +0900

    emacs用のパッケージ管理システムCaskをインストール

--- init.sh
+++ init.sh
@@ -7,4 +7,16 @@ else
      ln -s ${HOME}/dotfiles/emacs ${HOME}/.emacs.d
 fi
 
-
+if [ ! -e cask ]; then
+    curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+    mv ${HOME}/.cask ${HOME}/dotfiles/cask
+    ln -s ${HOME}/dotfiles/cask ${HOME}/.cask
+    export PATH="${HOME}/.cask/bin:$PATH"
+fi
+if [ ! -e ${HOME}/.emacs.d/Cask ]; then
+    cd ${HOME}/.emacs.d
+    cask upgrade
+    cask init
+fi
+cd ${HOME}/.emacs.d
+cask install
