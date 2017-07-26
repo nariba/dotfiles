e01f320242061e8b22e9b66807edefcf399b8e11
nariba <akira.naribayashi@gmail.com>
2017-07-26 20:45:56 +0900

    ツールバー、スクロールバーの設定を変更

--- emacs/conf/00-appearance.el
+++ emacs/conf/00-appearance.el
@@ -1,44 +1,58 @@
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format "%f")
+
+;; ツールバーの非表示
+(tool-bar-mode 0)
+
+;; スクロールバーの非表示
+(scroll-bar-mode 0)
+
+;; menu bar の非表示
+(menu-bar-mode 0)
+
 ;; カーソルの色を変更
-(if window-system
-    (progn
-      (if (fboundp 'mac-input-source)
-	  (progn
-	    (defun my-mac-selected-keyboard-input-source-change-function-cursor ()
-	      (let ((mac-input-source (mac-input-source)))
-		(set-cursor-color
-		 (if (or (string-match "com.apple.inputmethod.Kotoeri.Roman" mac-input-source)
-			 (string-match "com.apple.keylayout.US" mac-input-source))
-		     "white" "tomato1"))))
-	    ;; hook for cursor
-	    (add-hook 'mac-selected-keyboard-input-source-change-hook
-		      'my-mac-selected-keyboard-input-source-change-function)
-	    ;; timer for cursor
-	    (setq-default global-cursor-color-timer
-			  (run-with-idle-timer
-			   0.03
-			   t
-			   'my-mac-selected-keyboard-input-source-change-function-cursor))))))
+(when (window-system 'nil)
+  (if window-system
+      (progn
+	(if (fboundp 'mac-input-source)
+	    (progn
+	      (defun my-mac-selected-keyboard-input-source-change-function-cursor ()
+		(let ((mac-input-source (mac-input-source)))
+		  (set-cursor-color
+		   (if (or (string-match "com.apple.inputmethod.Kotoeri.Roman" mac-input-source)
+			   (string-match "com.apple.keylayout.US" mac-input-source))
+		       "white" "tomato1"))))
+	      ;; hook for cursor
+	      (add-hook 'mac-selected-keyboard-input-source-change-hook
+			'my-mac-selected-keyboard-input-source-change-function)
+	      ;; timer for cursor
+	      (setq-default global-cursor-color-timer
+			    (run-with-idle-timer
+			     0.03
+			     t
+			     'my-mac-selected-keyboard-input-source-change-function-cursor)))))))
 
 ;; カーソルのある行のハイライト色を変更
-(if window-system
-    (progn
-      (if (fboundp 'mac-input-source)
-	  (progn
-	    (defun my-mac-selected-keyboard-input-source-change-function_highlight ()
-	      (let ((mac-input-source (mac-input-source)))
-		(set-face-background hl-line-face
-				     (if (or (string-match "com.apple.inputmethod.Kotoeri.Roman" mac-input-source)
-					     (string-match "com.apple.keylayout.US" mac-input-source))
-					 "gray25" "tomato4"))))
-	    ;; hook for cursor
-	    (add-hook 'mac-selected-keyboard-input-source-change-hook
-		      'my-mac-selected-keyboard-input-source-change-function_highlight)
-	    ;; timer for cursor
-	    (setq-default global-cursor-color-timer
-			  (run-with-idle-timer
-			   0.03
-			   t
-			   'my-mac-selected-keyboard-input-source-change-function_highlight))))))
+(when (window-system 'nil)
+  (if window-system
+      (progn
+	(if (fboundp 'mac-input-source)
+	    (progn
+	      (defun my-mac-selected-keyboard-input-source-change-function_highlight ()
+		(let ((mac-input-source (mac-input-source)))
+		  (set-face-background hl-line-face
+				       (if (or (string-match "com.apple.inputmethod.Kotoeri.Roman" mac-input-source)
+					       (string-match "com.apple.keylayout.US" mac-input-source))
+					   "gray25" "tomato4"))))
+	      ;; hook for cursor
+	      (add-hook 'mac-selected-keyboard-input-source-change-hook
+			'my-mac-selected-keyboard-input-source-change-function_highlight)
+	      ;; timer for cursor
+	      (setq-default global-cursor-color-timer
+			    (run-with-idle-timer
+			     0.03
+			     t
+			     'my-mac-selected-keyboard-input-source-change-function_highlight)))))))
 
 ;; Mac上で軽くするための設定
 ;;; これはコメントアウトor取り除く
