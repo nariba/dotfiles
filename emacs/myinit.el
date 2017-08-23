2416e2f1cf25a9af29890ebb9ca20f1278d2b1b5
nariba <akira.naribayashi@gmail.com>
2017-08-23 22:51:03 +0900

    el-getの有効化

--- emacs/myinit.el
+++ emacs/myinit.el
@@ -1,10 +1,14 @@
 ;; Caskとpackageとpalletの連携
-(package-initialize)
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
+;; (package-initialize)
+;; (require 'cask "~/.cask/cask.el")
+;; (cask-initialize)
+;; (require 'pallet)
+;; (pallet-mode t)
+
+(add-to-list 'load-path "~/.emacs.d/el-get")
+(require 'el-get)
+(setq el-get-dir "~/.emacs.d/elisp")
 
 ;; init-loaderの起動、confディレクトリのelisp読み込み
-(require 'init-loader)
-(init-loader-load "~/.emacs.d/conf")
+;; (require 'init-loader)
+;; (init-loader-load "~/.emacs.d/conf")
