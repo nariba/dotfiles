d9077b5a318a18de502a428f27f4324280730833
nariba <akira.naribayashi@gmail.com>
2017-08-27 17:05:47 +0900

    端末での利用時にカラーテーマが設定されてしまうバグを修正

--- emacs/init.el
+++ emacs/init.el
@@ -11,7 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (misterioso)))
+ (when (window-system 'nil)
+   '(custom-enabled-themes (quote (misterioso))))
  '(package-selected-packages
    (quote
     (howm package-utils switch-window win-switch init-loader popup async bind-key cask dash diminish epl f flycheck git-commit magit-popup package-build pkg-info s shut-up with-editor auto-complete yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff)))
