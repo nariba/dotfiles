cf42fe2ad17be895985978d7466f6392418c6ca4
nariba <akira.naribayashi@gmail.com>
2017-09-03 02:09:35 +0900

    Remove cask's processes.

--- init.sh
+++ init.sh
@@ -7,20 +7,6 @@ else
      ln -s ${HOME}/dotfiles/emacs ${HOME}/.emacs.d
 fi
 
-if [ ! -e cask ]; then
-    curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-    mv ${HOME}/.cask ${HOME}/dotfiles/cask
-    ln -s ${HOME}/dotfiles/cask ${HOME}/.cask
-    export PATH="${HOME}/.cask/bin:$PATH"
-fi
-if [ ! -e ${HOME}/.emacs.d/Cask ]; then
-    cd ${HOME}/.emacs.d
-    cask upgrade
-    cask init
-fi
-cd ${HOME}/.emacs.d
-cask install
-
 # deploy git config files
 if [ -e ${HOME}/.gitconfig ]; then
     mv ${HOME}/.gitconfig ${HOME}/.gitconfig_org
