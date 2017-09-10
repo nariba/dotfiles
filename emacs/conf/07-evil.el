92c1b050c6313446caf237b49ab1675401b1b5cf
nariba <akira.naribayashi@gmail.com>
2017-09-10 15:03:31 +0900

    add the packages (mode-line-color and evil-line-mode)

--- emacs/conf/07-evil.el
+++ emacs/conf/07-evil.el
@@ -1,5 +1,9 @@
 ;; Enable evil
 (require 'evil)
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path "~/.emacs.d/elisp/evil-plugins")
+(require 'mode-line-color)
+(require 'evil-mode-line)
 (evil-mode 1)
 
 ;; (defun evil-escape-or-quit (&optional prompt)
