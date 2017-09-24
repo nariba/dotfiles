27dce8d253d3c6c857a9f319e89748c75bdfb63f
nariba <akira.naribayashi@gmail.com>
2017-09-24 17:53:38 +0900

    prezto のgit cloneを忘れていました…

--- init.sh
+++ init.sh
@@ -32,6 +32,7 @@ echo "zplug load --verbose" >> ${HOME}/.zshrc
 source ${HOME}/.zshrc
 zplug install
 rm ${HOME}/.zshrc
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zprezto
 
 setopt EXTENDED_GLOB
